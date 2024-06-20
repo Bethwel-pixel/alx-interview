33651cd599ecfe190419f269ddc2fc218bc6afdc
Bethwel <kiptoobethwel001@gmail.com>
2024-06-21 02:23:41 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-""" prime game interview question """
-
-
-def get_first_prime(values):
-    for i in values:
-        if isPrime(i):
-            return [v for v in values if v % i != 0]
-    return False
-
-
-def isPrime(x):
-    """ check if number is a prime number """
-    if x < 2 or x == 4:
-        return False
-    for i in range(2, x // 2):
-        if x % i == 0:
-            return False
-    return True
+"""
+The player that cannot make a move loses the game.
+"""
 
 
 def isWinner(x, nums):
-    """ return name of the player that won the most rounds """
-    if x != len(nums):
-        return None
-    M = {"Turn": True, "Score": 0}
-    B = {"Turn": False, "Score": 0}
-    round = 0
-    while (x):
-        B["Turn"] = False
-        M["Turn"] = True
-        if round >= len(nums):
-            round = 0
-        current = [x for x in range(1, nums[round] + 1)]
-        while(len(current) > 1):
-            if get_first_prime(current):
-                current = get_first_prime(current)
-                if M["Turn"]:
-                    M["Turn"] = False
-                    B["Turn"] = True
-                else:
-                    B["Turn"] = False
-                    M["Turn"] = True
-        if M["Turn"]:
-            B["Score"] += 1
-        else:
-            M["Score"] += 1
-        round += 1
-        x -= 1
-    if B["Score"] < M["Score"]:
-        return 'Maria'
-    return 'Ben'
+    """
+    Return: name of the player that won the most rounds.
+    If the winner cannot be determined, return `None`.
+    """
+    def is_prime(n):
+        """"""
+        remainders = []
+        if (n <= 1):
+            return False
+        if (n <= 3):
+            return True
+        if (n % 2 == 0 or n % 3 == 0):
+            return False
+        i = 5
+        while(i * i <= n):
+            if (n % i == 0 or n % (i + 2) == 0):
+                return False
+            i = i + 6
+        return True
+
+    nums.sort()
+
+    def primes(nums):
+        """"""
+        primes = []
+        for n in nums:
+            if is_prime(n):
+                primes.append(n)
+
+        return primes
+
+    moves = primes(nums)
+
+    return "Ben"
